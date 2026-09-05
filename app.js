6055fc2cd966ebf0634fe831775cf3a8432cfb9b
ambartsum26 <ambartsum.tigran@gmail.com>
2026-09-05 23:27:08 +0200

    revert: restore interface before 2026 UI refresh

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { startApplication } from './app-core.js';
+import './app-ui.js';
 import './app-popups.js';
 import './app-cards.js';
-import './app-ui.js';
 import './s13-export.js';
 
 function installJwBrand() {
