30501e023bcd13df070f0ca9a42f9215d4271a3b
ambartsum26 <ambartsum.tigran@gmail.com>
2026-09-06 07:36:29 +0200

    feat: enable territory status filtering

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { startApplication } from './app-core.js';
 import './app-ui.js';
 import './app-popups.js';
 import './app-cards.js';
+import './app-filters.js';
 import './s13-export.js';
 
 function installJwBrand() {
