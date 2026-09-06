01db669e0e4dfdef7b24ae236069bfa2cf008bb8
ambartsum26 <ambartsum.tigran@gmail.com>
2026-09-06 07:50:40 +0200

    chore: clean publisher roster migration imports

--- app-publisher-roster.js
+++ app-publisher-roster.js
@@ -4,7 +4,6 @@ import {
     doc,
     getDoc,
     getDocs,
-    onSnapshot,
     writeBatch
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
